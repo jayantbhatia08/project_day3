6ea7e4b71c442a95340eaca9eef6eed6d6a8b5f1
jayant.bhatia <jayantbhatia505@gmail.com>
2023-01-19 12:23:27 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.cyan,
+        appBar: AppBar(
+          title: const Center(
+              child: Text('Dice' , style: TextStyle(color: Colors.black,fontSize: 50,)),
+          ),
+          backgroundColor: Colors.yellow,
+        ),
+        body: Dice(),
+      ),
+    ),
+  );
+}
+
+class Dice extends StatefulWidget {
+  @override
+  _DiceState createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  int leftDice = 1;
+  int rightDice = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDice = Random().nextInt(6) + 1;
+      rightDice = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              child: Image.asset(
+                'images/dice$leftDice.png',
+              ),
+              onPressed: () {
+                changeDiceFace();
+              },
+            ),
+          ),
+          //Get students to create the second die as a challenge
+          Expanded(
+            child: TextButton(
+              child: Image.asset(
+                'images/dice$rightDice.png',
+              ),
+              onPressed: () {
+                changeDiceFace();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
